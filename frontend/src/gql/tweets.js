dc40b12dfc14ea84da6e94bbd67acca7ed31181e
Daniel Hayes <danihaye@gmail.com>
2020-08-30 18:13:27 -0400

    utilized graphql fragments to DRY up my gql code

--- frontend/src/gql/tweets.js
+++ frontend/src/gql/tweets.js
@@ -1,89 +1,80 @@
 import { gql } from '@apollo/client'
+import { loader } from 'graphql.macro'
+
+const profileFragment = loader('./fragments/profileFragment.graphql')
+const likeFragment = loader('./fragments/likeFragment.graphql')
+const retweetFragment = loader('./fragments/retweetFragment.graphql')
+const tweetFragment = loader('./fragments/tweetFragment.graphql')
 
 export const ALL_TWEETS = gql`
 query AllTweets {
   allTweets { 
-    id
-    text
-    profile {
-      id
-      user {
-        id
-        username
-      }
-    }
-    likes
+    ...TweetFragment
   }
 }
+${tweetFragment}
 `
 
 export const ALL_FOLLOWED_TWEETS = gql`
 query AllFollowedTweets {
   allFollowedTweets {
-    id
-    text
-    profile {
-      id
-      user {
-        id
-        username
-      }
-    }
-    likes
-  }
+    ...TweetFragment 
+  } 
 }
+${tweetFragment}
 `
 
 export const PROFILE_TWEETS = gql`
 query ProfileTweets($profile: String!) {
   profileTweets(profile: $profile) {
-    id
-    text
-    profile {
-      id
-      user {
-        id
-        username
-      }
-    }
-    likes
+    ...TweetFragment
   }
 }
+${tweetFragment}
+`
+
+export const ALL_FOLLOWED_TWEETS_AND_RETWEETS = gql`
+query AllFollowedTweetsAndRetweets {
+  allFollowedTweets {
+    ...TweetFragment
+  }
+  allFollowedRetweets {
+    ...RetweetFragment
+  }
+}
+${tweetFragment}
+${retweetFragment}
+${profileFragment}
+${likeFragment}
+`
+
+export const GET_TWEET = gql`
+query getTweet($id: Int!) {
+  getTweet(id: $id) {
+    ...TweetFragment
+  }
+}
+${tweetFragment}
 `
 
 export const COMPOSE_TWEET_MUTATION = gql`
 mutation CreateTweet($text: String!) {
   createTweet(text: $text) {
     tweet {
-      id 
-      text
-      profile {
-        id
-        user {
-          id
-          username
-        }
-      }
-      likes
+      ...TweetFragment
     }
   }
 }
+${tweetFragment}
 `
 
 export const LIKE_TWEET_MUTATION = gql`
 mutation LikeTweet($id: Int!) {
   likeTweet(id: $id) {
     tweet {
-      id
-      likes
-      text
-      profile {
-        user {
-          username
-        }
-      }
-      likes
+      ...TweetFragment
     }
   }
 }
+${tweetFragment}
 `
