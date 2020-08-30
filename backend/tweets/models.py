2346539c07ca0b4ec5d65b41927a105c9218667a
Daniel Hayes <danihaye@gmail.com>
2020-08-30 18:09:38 -0400

    changed likes to be their own model with a foreign key relationship to Tweet. added retweet model in a similar fashion, but also adding a new text field.

--- backend/tweets/models.py
+++ backend/tweets/models.py
@@ -1,24 +1,63 @@
 from django.db import models
+from django.core.validators import ValidationError
 from profiles.models import Profile
 
-# Create your models here.
+
 class Tweet(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     text = models.CharField(max_length=280)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    likes = models.IntegerField(default=1, blank=True, null=True)
 
-    def increment_likes(self):
-        self.likes = self.likes + 1
-        self.save()
+    def likes(self):
+        return Like.objects.filter(tweet=self)
 
-    def decrement_likes(self):
-        self.likes = self.likes - 1
-        self.save()
+    def retweets(self):
+        return Retweet.objects.filter(tweet=self)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        Like.objects.create(profile=self.profile, tweet=self)
 
     def __str__(self):
         return f'{self.profile.user.username}: {self.text}'
 
     def __repr__(self):
         return f'{self.profile.user.username}: {self.text}'
+
+
+class Like(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+
+    def clean(self):
+        exists = Like.objects.filter(profile=self.profile, tweet=self.tweet)
+        if exists:
+            raise ValidationError({
+                'profile': 'this profile has already liked this tweet.',
+                'tweet': 'this tweet was already liked by this profile',
+                })
+
+        return super().clean()
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+
+class Retweet(Tweet):
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE, related_name="retweets")
+
+    def clean(self):
+        exists = Retweet.objects.filter(profile=self.profile, tweet=self.tweet)
+        if exists:
+            raise ValidationError({
+                'profile': 'this profile has already retweeted this tweet.',
+                'tweet': 'this tweet was already retweeted by this profile',
+                })
+
+        return super().clean()
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
